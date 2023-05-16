b4d00367a33ec2fd979d1637b6c1b7ad21fe0295
Freddie Moala <loto_toa@hotmail.com>
2023-05-16 16:28:25 +1000

    Updated

---
+++ index.js
@@ -0,0 +1,37 @@
+// function foo() {
+//     return 'bar';
+// }
+
+// const foo = function() {
+//     return 'bar';
+// }
+
+// const add = parameter1 => parameter1 * 3;
+// console.log(add(2));
+
+// const sum = (parm1, pram2) => {
+//     console.log(`Adding ${parm1}`);
+//     console.log(`Adding ${pram2}`);
+//     return parm1 * pram2
+// }
+// console.log(sum(4, 5));
+
+// const nums = [1, 2, 3];
+// const squares = nums.map(x => x ** 2);
+
+// console.log(squares);
+// console.log(nums);
+
+const divide = function(pram1, pram2) {
+    return pram1 / pram2
+}
+console.log(divide(2000, 100));
+
+const square = (m) => {
+    return m * m;
+};
+
+const add = (pram3, pram4) => {
+    return pram3 + pram4;
+
+};
